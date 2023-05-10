4b200290dfc52da6e708a93d52df4b03076f9263
Ronaldoh1 <2ronald.hernandez@gmail.com>
2023-05-10 12:21:42 -0400

    added initial resources for pinch app

---
+++ PinchApp/PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by RONALD HERNANDEZ on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
